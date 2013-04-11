9765ec485f23d2c374cfcece9347511e26d88bbc
seferov <f.safarov@turkstudent.net>
2013-04-11 17:16:32 +0300

    [init] version 1.0

---
+++ background.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2013 Farhad Safarov <seferov@azerdict.com>
+
+/**
+ * Create a context menu
+ */
+chrome.contextMenus.create({
+  "title" : "translate",
+  "contexts": ["all"],
+  "type" : "normal",
+  "onclick" : getClickHandler()
+});
+
+/**
+ * Goes to a new page (azerdict.com)
+ */
+function getClickHandler() {
+  return function(info, tab) {
+
+    var azerdictUrl = "http://azerdict.com/";
+    var selectionText = info.selectionText;
+
+    if (selectionText) {
+      // The user selected some text, translate this
+      azerdictUrl += "english/" + encodeURI(selectionText) + "?utm_source=inline-search&utm_medium=chrome&utm_campaign=chrome-ext";
+    }
+
+    // Go to the page
+    chrome.tabs.create({"url" : azerdictUrl });
+  };
+
+};
